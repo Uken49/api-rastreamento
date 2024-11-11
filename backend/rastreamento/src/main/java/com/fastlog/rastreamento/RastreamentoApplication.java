3fc53f76246b4593dab53d6bc5928f4f4a3975c4
Uken49 <heldergavioes@hotmail.com>
2024-11-11 02:24:01 -0300

    build: configurando application.yml (não funcional)

--- backend/rastreamento/src/main/java/com/fastlog/rastreamento/RastreamentoApplication.java
+++ backend/rastreamento/src/main/java/com/fastlog/rastreamento/RastreamentoApplication.java
@@ -1,13 +1,29 @@
 package com.fastlog.rastreamento;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class RastreamentoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RastreamentoApplication.class, args);
+    }
+
+    @Component
+    public static class Runner implements ApplicationRunner {
+
+        @Value("${profiles.active}")
+        private String ambiente;
+
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            System.out.println("Aplicação rodando no ambiente: " + ambiente);
+        }
     }
 
 }
