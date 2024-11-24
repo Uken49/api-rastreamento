d7192fb3c5c98a39724f470f19d38624875fde15
Helder Davidson Rodrigues Alvarenga <70406114+Uken49@users.noreply.github.com>
2024-11-23 22:43:02 -0300

    Publicando imagem para o docker hub e conectando banco de prod (#7)

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,18 @@
-FROM openjdk:21 AS build
+FROM maven:3.9.9-eclipse-temurin-21 AS build
+WORKDIR /build
+COPY pom.xml .
+COPY src ./src
+RUN mvn clean package -DSPRING_APP_ENVIRONMENT=test -DskipTests
+
+FROM openjdk:21
 
 WORKDIR /app
 
-COPY target/api-rastreamento-1.0.0.jar api-rastreamento.jar
+COPY --from=build /build/target/api-rastreamento-*.jar api-rastreamento.jar
 
-ENTRYPOINT ["java", "-jar", "-DSPRING_APP_ENVIRONMENT=${SPRING_APP_ENVIRONMENT}", "/app/api-rastreamento.jar"]
+ENV SPRING_APP_ENVIRONMENT=prod
+ENV DATASOURCE_POSTGRES_PASSWORD=
+
+ENTRYPOINT ["java", "-jar", "-DSPRING_APP_ENVIRONMENT=${SPRING_APP_ENVIRONMENT}", "-DDATASOURCE_POSTGRES_PASSWORD=${DATASOURCE_POSTGRES_PASSWORD}","/app/api-rastreamento.jar"]
 
 EXPOSE 8000
--- terraform/network.tf
+++ terraform/network.tf
@@ -1,64 +1,174 @@
-# VPC
-resource "aws_vpc" "vpc_fastlog" {
-  cidr_block = "10.0.0.0/27"
+# Criar a VPC com o CIDR principal
+resource "aws_vpc" "vpc-fastlog" {
+  cidr_block = "10.0.0.0/26"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "vpc-fastlog"
+    Product     = "fastlog"
+    Environment = "prod"
+  }
+}
+
+# Associação para adicionar um CIDR secundário
+resource "aws_vpc_ipv4_cidr_block_association" "vpc-fastlog_additional_cidr" {
+  vpc_id     = aws_vpc.vpc-fastlog.id
+  cidr_block = "10.1.0.0/26"
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "igw_fastlog" {
-  vpc_id = aws_vpc.vpc_fastlog.id
+resource "aws_internet_gateway" "igw-fastlog" {
+  vpc_id = aws_vpc.vpc-fastlog.id
+
+  tags = {
+    Name        = "igw-fastlog"
+    Product     = "fastlog"
+    Environment = "prod"
+  }
 }
 
-resource "aws_eip" "eip_fastlog" {
-  associate_with_private_ip = false
-}
+resource "aws_eip" "eip-fastlog" {
+  for_each = {
+    "east-1a" = aws_subnet.public-fastlog-east_1a.id
+    "east-1b" = aws_subnet.public-fastlog-east_1b.id
+  }
 
-# Nat Gateway
-resource "aws_nat_gateway" "nat_fastlog" {
-  allocation_id     = aws_eip.eip_fastlog.id
-  subnet_id         = aws_subnet.subnet_public_fastlog.id
-  connectivity_type = "public"
-  depends_on        = [aws_internet_gateway.igw_fastlog]
+  tags = {
+    Name        = "eip-${each.key}"
+    Product     = "fastlog"
+    Environment = "prod"
+  }
 }
 
 # Subnets
-resource "aws_subnet" "subnet_public_fastlog" {
-  vpc_id                  = aws_vpc.vpc_fastlog.id
+# Public Subnets
+resource "aws_subnet" "public-fastlog-east_1a" {
+  vpc_id                  = aws_vpc.vpc-fastlog.id
   cidr_block              = "10.0.0.0/28"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "subnet_public-east_1a"
+    Product     = "fastlog"
+    Environment = "prod"
+  }
 }
 
-resource "aws_subnet" "subnet_private_fastlog" {
-  vpc_id            = aws_vpc.vpc_fastlog.id
+resource "aws_subnet" "public-fastlog-east_1b" {
+  vpc_id                  = aws_vpc.vpc-fastlog.id
+  cidr_block              = "10.1.0.0/28"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "subnet_public-east_1b"
+    Product     = "fastlog"
+    Environment = "prod"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private-fastlog-east_1a" {
+  vpc_id            = aws_vpc.vpc-fastlog.id
   cidr_block        = "10.0.0.16/28"
   availability_zone = "us-east-1a"
+
+  tags = {
+    Name        = "subnet_private-east_1a"
+    Product     = "fastlog"
+    Environment = "prod"
+  }
+}
+
+resource "aws_subnet" "private-fastlog-east_1b" {
+  vpc_id            = aws_vpc.vpc-fastlog.id
+  cidr_block        = "10.1.0.16/28"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name        = "subnet_private-east_1b"
+    Product     = "fastlog"
+    Environment = "prod"
+  }
+}
+
+# Nat Gateway
+resource "aws_nat_gateway" "nat-fastlog" {
+  for_each = {
+    "east-1a" = aws_subnet.public-fastlog-east_1a.id
+    "east-1b" = aws_subnet.public-fastlog-east_1b.id
+  }
+
+  allocation_id     = aws_eip.eip-fastlog[each.key].id
+  subnet_id         = each.value
+  connectivity_type = "public"
+
+  depends_on = [aws_internet_gateway.igw-fastlog]
+
+  tags = {
+    Name        = "nat-private-${each.key}"
+    Product     = "fastlog"
+    Environment = "prod"
+  }
 }
 
 # Route Tables
-resource "aws_route_table" "rt_public_fastlog" {
-  vpc_id = aws_vpc.vpc_fastlog.id
+resource "aws_route_table" "rt_public-fastlog" {
+  vpc_id = aws_vpc.vpc-fastlog.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw_fastlog.id
+    gateway_id = aws_internet_gateway.igw-fastlog.id
+  }
+
+  tags = {
+    Name        = "rt_public-fastlog"
+    Product     = "fastlog"
+    Environment = "prod"
   }
 }
 
-resource "aws_route_table" "rt_private_fastlog" {
-  vpc_id = aws_vpc.vpc_fastlog.id
+resource "aws_route_table" "rt_private-fastlog" {
+  for_each = {
+    "east-1a" = aws_nat_gateway.nat-fastlog["east-1a"].id
+    "east-1b" = aws_nat_gateway.nat-fastlog["east-1b"].id
+  }
+
+  vpc_id = aws_vpc.vpc-fastlog.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_fastlog.id
+    nat_gateway_id = each.value
+  }
+
+  tags = {
+    Name        = "rt_private-fastlog-${each.key}"
+    Product     = "fastlog"
+    Environment = "prod"
   }
 }
 
-resource "aws_route_table_association" "rta_public_fastlog" {
-  subnet_id      = aws_subnet.subnet_public_fastlog.id
-  route_table_id = aws_route_table.rt_public_fastlog.id
+# Associação para sub-redes públicas
+resource "aws_route_table_association" "rta_public-fastlog-east-1a" {
+  subnet_id      = aws_subnet.public-fastlog-east_1a.id
+  route_table_id = aws_route_table.rt_public-fastlog.id
 }
 
+resource "aws_route_table_association" "rta_public-fastlog-east-1b" {
+  subnet_id      = aws_subnet.public-fastlog-east_1b.id
+  route_table_id = aws_route_table.rt_public-fastlog.id
+}
+
+# Associação para sub-redes privadas
 resource "aws_route_table_association" "rta_private_fastlog" {
-  subnet_id      = aws_subnet.subnet_private_fastlog.id
-  route_table_id = aws_route_table.rt_private_fastlog.id
+  for_each = {
+    "east-1a" = aws_subnet.private-fastlog-east_1a.id
+    "east-1b" = aws_subnet.private-fastlog-east_1b.id
+  }
+
+  subnet_id      = each.value
+  route_table_id = aws_route_table.rt_private-fastlog[each.key].id
 }
